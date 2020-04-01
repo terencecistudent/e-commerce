5028ba8083d6506633956cbb6fe4074e5eee43e9
terencecistudent <terencelogue100@hotmail.co.uk>
2020-04-01 18:56:32 +0000

    Getting An AWS Account needs complete

---
+++ checkout/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import Order
+
+
+# Payment Form
+class MakePaymentForm(forms.Form):
+    MONTH_CHOICES = [(i, i) for i in range(1, 12)]
+    YEAR_CHOICES = [(i, i) for i in range(2017, 2036)]
+
+    credit_card_number = forms.CharField(label="Credit Card Number",
+                                         required=False)
+    cvv = forms.CharField(label="Security Code (CVV)", required=False)
+    expiry_month = forms.ChoiceField(label="Month", choices=MONTH_CHOICES,
+                                     required=False)
+    expiry_year = forms.ChoiceField(label="Month", choices=YEAR_CHOICES,
+                                    required=False)
+    stripe_id = forms.CharField(widget=forms.HiddenInput)
+
+
+# Order Form
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = (
+            "full_name", "phone_number", "country", "postcode",
+            "town_or_city", "street_address1", "street_address2",
+            "county"
+        )
